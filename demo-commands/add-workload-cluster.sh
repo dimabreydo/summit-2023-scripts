fc5ec260cdc76d84dcbb7661cb0a76625ab70733
Dima Breydo <dimabr@amazon.com>
2023-05-12 21:39:07 +0300

    scripts

--- demo-commands/add-workload-cluster.sh
+++ demo-commands/add-workload-cluster.sh
@@ -1,8 +1,8 @@
 cd ~/environment
 ./eks-multi-cluster-gitops/bin/add-cluster.sh gitops-system commercial-staging 
 
-cd ~/environment/gitops-system
-git pull —rebase origin main
-git add .
-git commit -m "Added cluster commercial-staging"
-git push 
+# cd ~/environment/gitops-system
+# git pull —rebase origin main
+# git add .
+# git commit -m "Added cluster commercial-staging"
+# git push 
