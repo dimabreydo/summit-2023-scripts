ea3cd740799160e06fdd6426e56e6250e88af79e
Dima Breydo <dimabr@amazon.com>
2023-05-17 12:20:48 +0300

    update scripts

--- demo-commands/connect-to-staging.sh
+++ demo-commands/connect-to-staging.sh
@@ -1,1 +1,2 @@
 source ~/environment/eks-multi-cluster-gitops/bin/connect-to-cluster.sh commercial-staging
+flux get kustomization
--- demo-commands/proxy-fe.sh
+++ demo-commands/proxy-fe.sh
@@ -1,1 +1,2 @@
+source ~/environment/eks-multi-cluster-gitops/bin/connect-to-cluster.sh commercial-staging
 kubectl -nproduct-catalog-fe port-forward svc/product-catalog-fe-staging 8080:80
